b5fdb1fe48427e585f544e4ba176b99fa379e02c
Archer <archer102125220.2015@gmail.com>
2020-04-11 18:58:55 +0800

    socket warning remove

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './app/index';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
 
 AppRegistry.registerComponent(appName, () => App);
